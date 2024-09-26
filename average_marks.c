669d1325cbfc7ca96c1d57447d06e8e65dc51fb7
lawrinda.obo <lawrinda.obo@amalitechtraining.org>
2024-09-26 10:57:33 +0000

    Added well done after congratulations

--- average_marks.c
+++ average_marks.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    int marks []= {40, 10, 75, 50, 60, 80};
+    int marks []= {40, 70, 75, 50, 60};
     int sum = 0;
      // Initialize sum to 0
 
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < 5; i++) {
         sum += marks[i]; // Accumulate the sum
     }
 
     printf("Your total marks = %d\n", sum); // Print the total sum
-    int average = sum/6;
+    int average = sum/5;
      printf("Average marks = %d\n",average);
+     printf("Congratulations, you passed, well done");
 
     return 0;
 }
