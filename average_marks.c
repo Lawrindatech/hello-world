b07c42f73c4075d08ee09d6860e58325a9a96187
lawrinda-obo <compliance@amalitech.org>
2024-09-26 10:27:22 +0000

    Added more marks

--- average_marks.c
+++ average_marks.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    int marks []= {40, 70, 75, 50, 60};
+    int marks []= {40, 10, 75, 50, 60, 80};
     int sum = 0;
      // Initialize sum to 0
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 6; i++) {
         sum += marks[i]; // Accumulate the sum
     }
 
     printf("Your total marks = %d\n", sum); // Print the total sum
-    int average = sum/5;
+    int average = sum/6;
      printf("Average marks = %d\n",average);
 
     return 0;
